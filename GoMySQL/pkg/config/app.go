4fcb7d71223c962e5fcaab317a2ada4af969e6cf
J4cKaLLL <j4ckal.r@gmail.com>
2022-07-23 14:46:35 -0500

    GetAllBooks

--- GoMySQL/pkg/config/app.go
+++ GoMySQL/pkg/config/app.go
@@ -14,7 +14,7 @@ var (
 func Connect() {
 	//dsn := "user:pass@tcp(127.0.0.1:3306)/dbname?charset=utf8mb4&parseTime=True&loc=Local"
 	d, err := gorm.Open("mysql", "root:Kaitoz0407@tcp(localhost:3306)/mysql?charset=utf8&parseTime=True&loc=Local")
-	fmt.Printf("Tipo de d %T\n y valor de %v", d, &d)
+	fmt.Printf("Tipo de d %T\ny valor de %v", d, &d)
 	if err != nil {
 		panic(err)
 	}
--- GoMySQL/pkg/routes/bookstore-routes.go
+++ GoMySQL/pkg/routes/bookstore-routes.go
@@ -5,9 +5,12 @@ import (
 	"github.com/gorilla/mux"
 )
 
+// Selecciona una ruta con el emparejador
+
 var RegisterBookStoreRoutes = func(router *mux.Router) {
 	//router.HandleFunc("/book/", controllers.CreateBook).Methods("POST")
 	router.HandleFunc("/book/", controllers.GetBook).Methods("GET")
+
 	// router.HandleFunc("/book/{bookId}", controllers.GetBookById).Methods("GET")
 	// router.HandleFunc("/book/{bookId}", controllers.UpdateBook).Methods("PUT")
 	// router.HandleFunc("/book/{bookId}", controllers.DeleteBook).Methods("DELETE")
